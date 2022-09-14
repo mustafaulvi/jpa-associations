89fcba6e16361d880482c92468fdb30f199d1cd1
mustafaulvi <ulvimustafa@gmail.com>
2022-09-14 09:31:33 +0200

    Created JPA Associations Projekt

---
+++ src/main/java/com/example/jpaassociations/JpaAssociationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaassociations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssociationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAssociationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpaassociations/JpaAssociationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaassociations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssociationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
